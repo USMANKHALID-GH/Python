770f93fc38872cdfa897b178dc1aa6e53d2fe73a
topba <66887206+topba@users.noreply.github.com>
2021-08-12 23:01:32 +0300

    python Structure

---
+++ venv/Include/DataTypes/__init__.py
@@ -0,0 +1,9 @@
+print(type(6))
+myLiss=[1,2,4,5,5,3,2,2,'usman']
+print(myLiss)
+myTuples=(1,2,4,5,3,2,2,'khalid')
+print(myTuples)
+myDictionary={1: 'usmankhalid', 2:4839}
+print(myDictionary)
+myset={1,2,4,4,3,2,2,2,'ususu'}
+print(myset)
