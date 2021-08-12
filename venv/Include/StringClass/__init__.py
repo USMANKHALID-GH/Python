327c441169b9386707fda4e298c4007fec1c20cc
topba <66887206+topba@users.noreply.github.com>
2021-08-12 23:58:52 +0300

    String

---
+++ venv/Include/StringClass/__init__.py
@@ -0,0 +1,16 @@
+name='usman'
+age=int(56)
+message=' Hello my name  is {} and my age is {}'.format(name.upper(),age)
+print(message)
+# len()
+String='checking the length'
+print(len(String))
+
+#count()
+print(message.count('a'))
+
+#strip()
+print(message.strip())
+print(','.join(name))
+print(message.title())
+print(message.swapcase())
